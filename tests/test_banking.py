57c4e4dbfa69ce6bb06744298af97b35eb00058d
Becky <55168393+lunaxtasy@users.noreply.github.com>
2019-11-04 02:40:30 -0500

    Assignment 5: Step 4

--- tests/test_banking.py
+++ tests/test_banking.py
@@ -6,6 +6,7 @@ This file contains all the test scenerios checked for Assignment 5
 
 #import pytest - not needed due to using virtual environment on PC
 import datetime
+from freezegun import freeze_time
 from banking import Transaction
 
 def test_account_setup():
@@ -40,3 +41,12 @@ def test_timestamp():
 
     becky_account = Transaction(100, datetime.datetime(2018, 1, 1, 1, 1, 1))
     assert becky_account.get_timestamp() == datetime.datetime(2018, 1, 1, 1, 1, 1)
+
+@freeze_time("2019-03-15")
+def test_no_timestamp():
+    """
+    This test verifies that no timestamps default to the current time
+    """
+
+    becky_account = Transaction(100.01)
+    assert becky_account.get_timestamp() == datetime.datetime(2019, 3, 15)
